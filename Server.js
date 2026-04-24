8d17e05353eb5104602ac293a297f56b22b7f18b
Pravallika2521 <pravallikagumudavelli@gmail.com>
2026-04-24 07:21:35 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,52 +1,14 @@
-const express = require("express");
-const axios = require("axios");
+const express = require('express');
+const dotenv = require('dotenv');
+const releaseRoutes = require('./routes/releaseRoutes');
+ 
+dotenv.config();
  
 const app = express();
  
-// Replace with your details
-const JIRA_URL = "https://pravallikagumudavelli.atlassian.net";
-const EMAIL = "pravallikagumudavelli@gmail.com";
-const API_TOKEN = ATATT3xFfGF00V5b_p35QFDajwzG55hZFN4cQqVDyWpk8wDuvluAS8i1mx7VT0elkM2tPgf83Hl3q8wp8v3O2h0chy5XhjzutUF0jNOrufwVkN6xrSWMH-nq4DWHHJU5nsZ4J_00dikL4JpvxERQcjjk3E6mESKgSCwSYhpjSZNhAkyJ65BDekE=A839F050;
+app.use('/api', releaseRoutes);
  
-app.get("/jira-data", async (req, res) => {
-  try {
-    const response = await axios.get(
-      `${JIRA_URL}/rest/api/3/search`,
-      {
-        auth: {
-          username: EMAIL,
-          password: API_TOKEN
-        }
-      }
-    );
- 
-    const issues = response.data.issues;
- 
-    let openBugs = 0;
-    let doneTasks = 0;
- 
-    issues.forEach(issue => {
-      if (issue.fields.issuetype.name === "Bug" &&
-          issue.fields.status.name !== "Done") {
-        openBugs++;
-      }
- 
-      if (issue.fields.status.name === "Done") {
-        doneTasks++;
-      }
-    });
- 
-    res.json({
-      totalIssues: issues.length,
-      openBugs,
-      doneTasks
-    });
- 
-  } catch (error) {
-    res.status(500).send("Error fetching Jira data");
-  }
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
 });
  
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
